57a122c85bcf5f96275a5ff0b0073c0516c04ff1
JagritGill <72464099+JagritGill@users.noreply.github.com>
2021-07-28 10:23:58 -0700

    Update Main.java
    answered the following question.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
+import java.util.Scanner:
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "what color is the sky?";
+        String choiceOne = "green";
+        String choiceTwo = "blue";
+        String choiceThree = "red";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
+        
         // Write a print statement giving the answer choices
+        System.out.println("you have three choices :"
+                          + "choice One = " + choiceOne +
+                          "choice Two = " + choiceTwo +
+                          "choice Three = " + choiceThree +
+                          "please type in the color you think it is.");
+        
+        
 
         // Have the user input an answer
+        Scanner input = new Scanner(System.in);
+        String userInput = input.next;
         // Retrieve the user's input
 
         // If the user's input matches the correctAnswer...
+        if(userInput == correctAnswer){
+            System.out.println("you are correct!!!");
+        }else{
+            System.out.println("sorry try again next time.");
+        }
         // then the user is correct and we want to print out a congrats message to the user.
 
         // If the user's input does not match the correctAnswer...
